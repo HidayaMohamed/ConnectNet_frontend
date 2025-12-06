84fb8e77abce0c0f03abd9778e385beaae142c63
Hidaya Mohamed <hidayamohamed002@gmail.com>
2025-12-06 20:37:25 +0300

    Added a profile page

--- src/pages/Profile.jsx
+++ src/pages/Profile.jsx
@@ -1,1 +1,17 @@
 
+import { useParams } from "react-router-dom";
+import FollowButton from "../components/FollowButton";
+
+export default function Profile() {
+  const { id } = useParams(); // user id from URL
+
+  return (
+    <div className="p-4 max-w-xl mx-auto">
+      <h1 className="text-2xl font-bold mb-2">User Profile #{id}</h1>
+
+      <FollowButton userId={1} targetUserId={id} />
+
+      
+    </div>
+  );
+}
