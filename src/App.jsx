3fe8448bd0aa1adfee6abb07ea64ddec91439e4f
Hidaya Mohamed <hidayamohamed002@gmail.com>
2025-12-06 12:34:41 +0300

    Updated app.jsx

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import Navbar from "./components/Navbar";
+import Home from "./pages/Home";
+
+function App() {
+  return (
+    <div className="bg-gray-100 min-h-screen">
+      <Navbar />
+      <Home />
+    </div>
+  );
+}
+
+export default App;
